69a334f6e8162da313b9a68e2f76eea215689bf6
way-huang <40635359+way-huang@users.noreply.github.com>
2023-02-22 13:55:13 +0800

    Update iproxy.rb
    make sha256 to no_check

--- Casks/iproxy.rb
+++ Casks/iproxy.rb
@@ -1,6 +1,6 @@
 cask "iproxy" do
   version "nightly"
-  sha256 "66b6984b1a3fbebb5f9a9eb1647a4ee375cfb04dd2eb7a40fb6b83dba0cf37d0"
+  sha256 :no_check
 
   url "https://nightly.link/xcodebuild/iProxy/workflows/nightly/master/iProxyNightly-macOS.zip"
   name "iProxy"
