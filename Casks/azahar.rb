ae1262b8ea7037bab8b0f6d7a222db33cbb1f171
Way <way.h@outlook.com>
2025-07-06 19:43:51 +0800

    feat: add Azahar 3ds Emulator

---
+++ Casks/azahar.rb
@@ -0,0 +1,11 @@
+cask 'azahar' do
+  version '2122.1'
+  sha256 'a9adf930747b65b962003fc0057c2dc95ebe0872a51e29e451785e9a9f34101b'
+
+  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip"
+  desc 'Azahar is an open-source 3DS emulator based on Citra'
+  name 'Azahar Emulator'
+  homepage 'https://azahar-emu.org/'
+
+  app 'Azahar.app'
+end
