6ff1b84d215974ccea34166d03f969ba43983ac9
way <way.h@outlook.com>
2025-10-04 19:09:49 +0800

    feat: add siyuan notes patch

---
+++ Casks/siyuan-notes@patch.rb
@@ -0,0 +1,21 @@
+cask "siyuan-notes@patch" do
+  version "3.3.4"
+  sha256 :no_check
+
+  on_arm do
+    url "https://github.com/demoshang/siyuan-patch/releases/download/v#{version}/siyuan-v#{version}-mac-arm64.dmg"
+  end
+  on_intel do
+    url "https://github.com/demoshang/siyuan-patch/releases/download/v#{version}/siyuan-v#{version}-mac.dmg"
+  end
+
+  name "SiYuan"
+  desc "an open source notes"
+  homepage "https://github.com/demoshang/siyuan-patch"
+
+  app "SiYuan.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "/Applications/SiYuan.app"]
+  end
+end
