77f63e413e28a07b7a9bda2def189bf5d4a004ad
way <way.h@outlook.com>
2025-12-09 14:15:32 +0800

    feat: add dash@4

---
+++ Casks/dash@4.rb
@@ -0,0 +1,34 @@
+cask "dash@4" do
+  version "4.6.7"
+  sha256 "e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6"
+
+  url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
+  name "Dash"
+  desc "API documentation browser and code snippet manager"
+  homepage "https://kapeli.com/dash"
+
+  livecheck do
+    url "https://kapeli.com/Dash4.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  conflicts_with cask: [
+    "dash",
+    "dash@6"
+  ]
+
+  app "Dash.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kapeli.dashdoc",
+    "~/Library/Application Support/Dash",
+    "~/Library/Caches/com.kapeli.dashdoc",
+    "~/Library/Cookies/com.kapeli.dashdoc.binarycookies",
+    "~/Library/HTTPStorages/com.kapeli.dashdoc.binarycookies",
+    "~/Library/Logs/Dash",
+    "~/Library/Preferences/com.kapeli.dashdoc.plist",
+    "~/Library/Saved Application State/com.kapeli.dashdoc.savedState",
+    "~/Library/WebKit/com.kapeli.dashdoc",
+  ]
+end
