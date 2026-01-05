9c17c9d5d8901019bb103aa4a106012d81d806e7
way <way.h@outlook.com>
2026-01-05 16:00:46 +0800

    Create things3.rb

---
+++ Casks/things3.rb
@@ -0,0 +1,10 @@
+cask "things3" do
+  version "3.22.7"
+  sha256 :no_check
+  url "https://static.culturedcode.com/things/Things3.zip"
+  name "Things"
+  desc "Things is the award-winning personal task manager that helps you plan your day, manage your projects, and make real progress toward your goals."
+  homepage "https://culturedcode.com/things/"
+  auto_updates true
+  app "Things3.app"
+end
