79746c71e4c4e363e2ebfcbdf7a17e1da54814c4
way <way.h@outlook.com>
2026-05-16 14:05:57 +0800

    fix url

--- Casks/azahar.rb
+++ Casks/azahar.rb
@@ -2,7 +2,7 @@ cask 'azahar' do
   version '2125.1.2'
   sha256 'sha256:5d3aedc3840cf3b536caea9e9b60811e6c3a07475e2e5a36d816ffcbd58eecb4'
 
-  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip"
+  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-macos-universal-#{version}.zip"
   desc 'Azahar is an open-source 3DS emulator based on Citra'
   name 'Azahar Emulator'
   homepage 'https://azahar-emu.org/'
